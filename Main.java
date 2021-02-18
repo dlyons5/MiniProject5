4b1b482addb1a62ea7786b0000fe4cbeab3b4a45
dlyons5 <dominic.lyons@patelhs.org>
2021-02-18 14:48:23 +0000

    idea: counting the chips in a Pringles can

--- Main.java
+++ Main.java
@@ -1,4 +1,18 @@
 class Main {
-  public static void main(String[] args) {
-   
   
+  public static void main(String[] args) {
+    numChips(100);
+  }
+//100 is the number of chips in a pringles can 
+  public static void numChips(int n) {
+    if (n == 0){
+       System.out.println("Done");
+    }else{
+      System.out.println(n);
+      n--;
+      numChips(n);
+    }
+  }
+}
+
+

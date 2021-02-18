cfc81ff521b1da7efa12b9fd3217ff5ab471d2b0
dlyons5 <dominic.lyons@patelhs.org>
2021-02-18 15:29:51 +0000

    stuck

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 class Main {
   
   public static void main(String[] args) {
-    int result = sum(15);
+    
+    int result = sum(13);
     System.out.println(result);
     numChips(100);
+   
   }
 
-//100 is the number of chips in a pringles can 
+//there is on average 100 chips in a pringles can
   public static void numChips(int n) {
     if (n == 0){
        System.out.println("Done");
@@ -17,22 +19,16 @@ class Main {
     }
 }
 
-
-//if I receive 5 as a parameter
-  //I want to add 5 + 4 + 3 + 2 + 1
+//
   public static int sum(int num)
   {
     if(num > 0)
     {
       return num + sum(num - 1);
-      //5 + 10 = 15 
-      //4 + 6 = 10
-      //3 + 3 = 6
-      //2 + 1 = 3
-      //1 + 0 = 1
+      // 
     }else{
       return 0;
-      //with 0 as our parameter, we get zero back
+      //
    
    
     }
